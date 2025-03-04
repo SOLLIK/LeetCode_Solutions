879a361cba4527c6da2abde8339d545a6f794066
Ivan <129214683+SOLLIK@users.noreply.github.com>
2025-03-04 12:26:13 +0200

    Time: 0 ms (100.00%) | Memory: 23.6 MB (72.39%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -11,33 +11,23 @@
  */
 class Solution {
 public:
-    int diameterOfBinaryTree(TreeNode* root) {
-        unordered_map<TreeNode*, pair<int, int>> mp;
-        mp[nullptr] = {0, 0};
-        stack<TreeNode*> stack;
-        stack.push(root);
-
-        while(!stack.empty()) {
-            TreeNode* node = stack.top();
-            if(node->left && mp.find(node->left) == mp.end()) {
-                stack.push(node->left);
-            }
-            else if (node->right && mp.find(node->right) == mp.end()) {
-                stack.push(node->right);
-            }
-            else {
-                node = stack.top();
-                stack.pop();
-
-                auto[leftHeight, leftDiametr] = mp[node->left];
-                auto[rightHeight, rightDiametr] = mp[node->right];
-
-                int height = 1 + max(leftHeight, rightHeight);
-                int diametr = max(leftHeight + rightHeight, max(leftDiametr, rightDiametr));
-
-                mp[node] = {height, diametr};
-            }
+    int func(TreeNode* root, int* mp) {
+        if(root == nullptr) {
+            return 0;
         }
-        return mp[root].second;
+
+        int left = func(root->left, mp);
+        int right = func(root->right, mp);
+
+        *mp = max(*mp, left + right);
+        return 1 + max(left, right);
+    }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        int res = 0;
+
+        func(root, &res);
+        
+        return res;
     }
 };
