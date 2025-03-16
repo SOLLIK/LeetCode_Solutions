8fa1ce0312d26c251d86563d1b59831faa47ec4f
Ivan <129214683+SOLLIK@users.noreply.github.com>
2025-03-16 13:42:39 +0200

    Time: 121 ms (99.17%) | Memory: 146 MB (96.73%) - LeetSync

--- 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.cpp
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.cpp
@@ -2,24 +2,22 @@ class WordDictionary {
 private:
     unordered_map<int, unordered_set<string>> hashmap;
 public:
-    WordDictionary() {
-        
-    }
+    WordDictionary() {}
     
     void addWord(string word) {
         int n = word.size();
         hashmap[n].insert(word);
-        if(!hashmap[-2].empty()){hashmap[-2].clear();}
+        if(!hashmap[-2].empty())hashmap[-2].clear();
         return;
     }
     
     bool search(string word) {
         int n = word.size();
-        if(hashmap[n].count(word)||hashmap[-1].count(word)){return true;}
-        if(hashmap[-2].count(word)){return false;}
+        if(hashmap[n].count(word)||hashmap[-1].count(word))return true;
+        if(hashmap[-2].count(word))return false;
         for(const auto& s:hashmap[n]){
             for(int i=0;i<n;++i){
-                if(word[i]!=s[i] && word[i]!='.'){break;}
+                if(word[i]!=s[i] && word[i]!='.')break;
                 if(i==n-1){
                     hashmap[-1].insert(word);
                     return true;
