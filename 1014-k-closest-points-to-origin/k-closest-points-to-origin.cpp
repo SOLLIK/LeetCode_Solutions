0b15e9545419fb644a2336ff5eec44e63840d86e
Ivan <129214683+SOLLIK@users.noreply.github.com>
2025-03-18 14:54:16 +0200

    Time: 27 ms (87.52%) | Memory: 67.6 MB (76.52%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,36 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        int L = 0, R = points.size() - 1;
-        int pivot = points.size();
-
-        while (pivot != k) {
-            pivot = partition(points, L, R);
-            if (pivot < k) {
-                L = pivot + 1;
-            } else {
-                R = pivot - 1;
+        priority_queue<pair<int, pair<int, int>>> maxHeap;
+        for(auto& point : points) {
+            int dist = point[0] * point[0] + point[1] * point[1];
+            maxHeap.push({dist, {point[0], point[1]}});
+            if(maxHeap.size() > k) {
+                maxHeap.pop();
             }
         }
-        return vector<std::vector<int>>(points.begin(), points.begin() + k);
-    }
 
-private:
-    int partition(vector<vector<int>>& points, int l, int r) {
-        int pivotIdx = r;
-        int pivotDist = euclidean(points[pivotIdx]);
-        int i = l;
-        for (int j = l; j < r; j++) {
-            if (euclidean(points[j]) <= pivotDist) {
-                swap(points[i], points[j]);
-                i++;
-            }
+        vector<vector<int>> res;
+        while(!maxHeap.empty()) {
+            res.push_back({maxHeap.top().second.first, maxHeap.top().second.second});
+            maxHeap.pop();
         }
-        swap(points[i], points[r]);
-        return i;
-    }
-
-    int euclidean(vector<int>& point) {
-        return point[0] * point[0] + point[1] * point[1];
+        return res;
     }
 };
